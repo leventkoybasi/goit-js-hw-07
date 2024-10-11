89d80fdf8f69c88d00b3cc9b6bcb6c8d04667240
Levent KÖYBAŞI <leventkoybasi@hotmail.com>
2024-10-10 17:44:20 -0700

    completed task-5

--- js/task-5.js
+++ js/task-5.js
@@ -6,3 +6,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const changeColorBtn = document.querySelector(".change-color");
+const spanColor = document.querySelector(".color");
+
+changeColorBtn.addEventListener("click", () => {
+  const newColor = getRandomHexColor();
+  document.querySelector("body").style.backgroundColor = newColor;
+  spanColor.textContent = newColor;
+});
