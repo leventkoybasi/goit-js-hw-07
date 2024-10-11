c4d4410c94e8964d95f128c8e585e5d542cf5c89
Levent KÖYBAŞI <leventkoybasi@hotmail.com>
2024-10-10 17:02:40 -0700

    completed task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,2 +1,21 @@
 "use strict";
 console.log("%cTask-4 Buradan Baslar", "color: red");
+
+const formLogin = document.querySelector(".login-form");
+
+formLogin.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const userEmail = document.querySelector('input[type="email"]').value;
+  const password = document.querySelector('input[type="password"]').value;
+
+  if (userEmail.length > 0 && password.length > 0) {
+    const userInfo = {
+      email: userEmail.trim(),
+      password: password.trim(),
+    };
+    console.table(userInfo);
+    document.querySelector(".login-form").reset();
+  } else {
+    alert("All form fields must be filled in");
+  }
+});
